493eac0463a8667fa90b342c1f52884179c30bb3
tonytor <teme4842@gmail.com>
2024-08-03 20:51:36 +0300

    install nextjs and setup scss extension for styleing purpose

---
+++ inventory-software/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex justify-center items-center min-h-screen">
+      <h2 className="font-bold text-2xl">Inventory Management System</h2>
+   </div>
+  );
+}
